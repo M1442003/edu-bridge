b8ddf2384b6a329c59391c7a4ca88055db201be9
Matolacrypto <husseinmatolakaunga@gmail.com>
2026-01-16 12:19:38 +0300

    fix: update JwtStrategy to use environment variable for secret key and improve return structure in validate method

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -7,11 +7,15 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'your_jwt_secret',
+      secretOrKey: process.env.JWT_SECRET,
     });
   }
 
   async validate(payload: any) {
-    return { userId: payload.sub, role: payload.role };
+    return {
+      id: payload.sub,
+      role: payload.role,
+    };
   }
 }
+
--- src/auth/roles.guard.ts
+++ src/auth/roles.guard.ts
@@ -29,4 +29,5 @@ export class RolesGuard implements CanActivate {
 
     return requiredRoles.includes(user.role);
   }
+
 }
